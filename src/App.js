16114f828a3d4a92a7099af38e0cd5fbede66668
ChinniRajulapati <rchinni98@gmail.com>
2023-03-09 16:16:25 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+import {Route, Switch} from 'react-router-dom'
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Header from './components/Header'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <Route exact path="/" component={Home} />
+    <Route component={NotFound} />
+  </Switch>
+)
+
+export default App
